8dbfb9c7ec62b91240a4832658bce6f8e09089a7
Samuel Hulme <41990982+ajh123@users.noreply.github.com>
2023-10-06 11:40:16 +0000

    VS code runs and  begin py4j

--- common/src/main/java/uk/minersonline/pyminersloader/PyMinersLoader.java
+++ common/src/main/java/uk/minersonline/pyminersloader/PyMinersLoader.java
@@ -1,10 +1,19 @@
 package uk.minersonline.pyminersloader;
 
+import py4j.GatewayServer;
+
 public class PyMinersLoader
 {
 	public static final String MOD_ID = "pyminersloader";
 
+    public PythonEntryPoint() {
+
+	}
+  
+
 	public static void init() {
-		
+        GatewayServer gatewayServer = new GatewayServer(new PythonEntryPoint());
+        gatewayServer.start();
+        System.out.println("Gateway Server Started");		
 	}
 }
